6c65d15ffa1c9e9019a9e51e302e19f451b9bc12
Pranab Shukla <pranab.austin@gmail.com>
2023-02-07 22:00:08 -0600

    update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(express.json())
 app.use(express.static("static"))
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://pfmnrtg5a3m9tjr62cwn.vercel.app/');
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
